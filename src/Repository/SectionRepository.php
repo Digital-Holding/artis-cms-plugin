9d96cc700b1e1b1acd8f1eade63df8c2166740f5
mati <mateusz.koruna@digitalholding.pl>
2021-08-02 15:00:03 +0200

    added taxonable sections endpoint

---
+++ src/Repository/SectionRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DH\ArtisCmsPlugin\Repository;
+
+use BitBag\SyliusCmsPlugin\Repository\SectionRepository as BaseSectionRepository;
+
+class SectionRepository extends BaseSectionRepository implements SectionRepositoryInterface
+{
+    public function findByTaxonable(bool $taxonable): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.taxonable = :taxonable')
+            ->setParameter('taxonable', $taxonable)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+}
