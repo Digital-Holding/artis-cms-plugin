e48b246f3489841d49648598b0083c02d1f612d6
Soft Well House <bialonj@gmail.com>
2021-10-11 09:32:54 +0200

    endpoint

--- src/Controller/Api/Catalog/ShowCatalogByCode.php
+++ src/Controller/Api/Catalog/ShowCatalogByCode.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace DH\ArtisCmsPlugin\Controller\Api\Catalog;
 
+use DH\ArtisCmsPlugin\Entity\CatalogInterface;
+use DH\ArtisCmsPlugin\Factory\ViewFactory\Category\CatalogViewFactory;
 use DH\ArtisCmsPlugin\Repository\CatalogRepositoryInterface;
+use DH\ArtisCmsPlugin\View\Catalog\CatalogView;
+use DH\ArtisCmsPlugin\View\Page\PageView;
+use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandlerInterface;
+use Sylius\ShopApiPlugin\Http\RequestBasedLocaleProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class ShowCatalogByCode
 {
@@ -13,11 +22,54 @@ final class ShowCatalogByCode
 
     private CatalogRepositoryInterface $catalogRepository;
 
+    private RequestBasedLocaleProviderInterface $requestBasedLocaleProvider;
+
+    private CatalogViewFactory $catalogViewFactory;
+
     public function __construct(
         ViewHandlerInterface $viewHandler,
-        CatalogRepositoryInterface $catalogRepository
+        CatalogRepositoryInterface $catalogRepository,
+        RequestBasedLocaleProviderInterface $requestBasedLocaleProvider,
+        CatalogViewFactory $catalogViewFactory
     ) {
         $this->viewHandler = $viewHandler;
         $this->catalogRepository = $catalogRepository;
+        $this->requestBasedLocaleProvider = $requestBasedLocaleProvider;
+        $this->catalogViewFactory = $catalogViewFactory;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        if ($request->attributes->has('year')) {
+            $year = $request->attributes->get('year');
+        } else {
+            $datetime = new \DateTime('now');
+            $year = $datetime->format('Y');
+        }
+
+        $localeCode = $this->requestBasedLocaleProvider->getLocaleCode($request);
+
+        /** @var CatalogInterface $catalog */
+        $catalog = $this->catalogRepository->findOneEnabledByCode(
+            $request->attributes->get('menuCode'),
+            $year,
+            $localeCode
+        );
+
+        if (null === $catalog) {
+            throw new NotFoundHttpException(sprintf('Catalog has not been found.'));
+        }
+
+        return $this->viewHandler->handle(
+            View::create(
+                $this->buildView($catalog, $localeCode),
+                Response::HTTP_OK
+            )
+        );
+    }
+
+    private function buildView(CatalogInterface $catalog, string $localeCode): CatalogView
+    {
+        return $this->catalogViewFactory->create($catalog, $localeCode);
     }
 }
